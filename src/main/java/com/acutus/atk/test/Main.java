337eb256c1f6c0f126e5633b52213263678fa83e
jaspervdbijl <jaspervdb@spsi.co.za>
2018-12-26 12:48:19 +0200

    Entity implementation now extends form the Source (to include all methods in base class ) Added Query methods in Entity implementation

--- src/main/java/com/acutus/atk/test/Main.java
+++ src/main/java/com/acutus/atk/test/Main.java
@@ -3,7 +3,7 @@ package com.acutus.atk.test;
 public class Main {
 
     public static void main(String[] args) {
-        com.acutus.atk.test.PersonEntity p = new com.acutus.atk.test.PersonEntity();
+        PersonEntity p = new PersonEntity();
         System.out.println(p._name.set("ello").getName());
         System.out.println(p.getFields().getChanged().toString());
 
